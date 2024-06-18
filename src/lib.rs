22666f8e77a7b7b2d92a008037295dd205ffa653
Marshall Ku <marshall@kakao.com>
2024-06-18 19:12:57 +0900

    Implement logic for decompressing data

--- src/lib.rs
+++ src/lib.rs
@@ -29,5 +29,31 @@ pub fn compress_sequence(sequence: &str) -> Vec<u8> {
     compressed
 }
 
+pub fn decompress_sequence(compressed: &[u8], length: usize) -> String {
+    let mut sequence = String::new();
+    let mut bits = 0;
+    let mut bit_count = 0;
+
+    for &byte in compressed {
+        bits = (bits << 8) | byte as usize;
+        bit_count += 8;
+
+        while bit_count >= 2 && sequence.len() < length {
+            let base_bits = (bits >> (bit_count - 2)) & 0b11;
+            let base = match base_bits {
+                0b00 => 'A',
+                0b01 => 'C',
+                0b10 => 'T',
+                0b11 => 'G',
+                _ => panic!("Invalid bits!"),
+            };
+            sequence.push(base);
+            bit_count -= 2;
+        }
+    }
+
+    sequence
+}
+
 #[cfg(test)]
 mod tests {}
