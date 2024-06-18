cd0b4fb03835375b959981016e55f52aba4c0d05
Marshall Ku <marshall@kakao.com>
2024-06-18 19:12:39 +0900

    Implement logic for compressing data

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn compress_sequence(sequence: &str) -> Vec<u8> {
+    let mut compressed = Vec::new();
+    let mut current_byte = 0u8;
+    let mut bit_count = 0;
+
+    for base in sequence.chars() {
+        let bits = match base {
+            'A' => 0b00,
+            'C' => 0b01,
+            'T' => 0b10,
+            'G' => 0b11,
+            _ => panic!("Invalid SEQUENCE!"),
+        };
+
+        current_byte = (current_byte << 2) | bits;
+        bit_count += 2;
+
+        if bit_count == 8 {
+            compressed.push(current_byte);
+            current_byte = 0;
+            bit_count = 0;
+        }
+    }
+
+    if bit_count > 0 {
+        compressed.push(current_byte << (8 - bit_count));
+    }
+
+    compressed
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
