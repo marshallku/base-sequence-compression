0623db1b61d6f79fd6d1bed8f7ef45629de9722f
Marshall Ku <marshall@kakao.com>
2024-12-20 12:59:27 +0900

    Add benchmark for decompressing logic

--- benches/main.rs
+++ benches/main.rs
@@ -1,8 +1,8 @@
-use std::path::Path;
+use std::{fs::File, io::Read, path::Path};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use base_sequence_compression::compress_fasta;
+use base_sequence_compression::{compress_fasta, decompress_fasta};
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Compress Small FASTA file", |b| {
@@ -17,6 +17,24 @@ fn criterion_benchmark(c: &mut Criterion) {
             let input_path = Path::new("benches/input/large.fasta");
             let content = black_box(std::fs::read_to_string(input_path).unwrap());
             compress_fasta(black_box(&content));
+        })
+    });
+    c.bench_function("Decompress Small FASTA file", |b| {
+        b.iter(|| {
+            let path = Path::new("benches/output/test.bin");
+            let mut file = File::open(path).ok().unwrap();
+            let mut content = Vec::new();
+            file.read_to_end(black_box(&mut content)).ok();
+            decompress_fasta(black_box(&content));
+        })
+    });
+    c.bench_function("Decompress Large FASTA file", |b| {
+        b.iter(|| {
+            let path = Path::new("benches/output/large.bin");
+            let mut file = File::open(path).ok().unwrap();
+            let mut content = Vec::new();
+            file.read_to_end(black_box(&mut content)).ok();
+            decompress_fasta(black_box(&content));
         })
     });
 }
