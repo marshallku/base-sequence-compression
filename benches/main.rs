2b9e9d966aad8a720eface5c4191cc8d6860f93b
Marshall Ku <marshall@kakao.com>
2024-12-20 12:57:03 +0900

    Fix benchmark

--- benches/main.rs
+++ benches/main.rs
@@ -2,27 +2,21 @@ use std::path::Path;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use base_sequence_compression::{compress_fasta, decompress_fasta};
+use base_sequence_compression::compress_fasta;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("Small FASTA file", |b| {
+    c.bench_function("Compress Small FASTA file", |b| {
         b.iter(|| {
             let input_path = Path::new("benches/input/test.fasta");
             let content = black_box(std::fs::read_to_string(input_path).unwrap());
-            let content = compress_fasta(black_box(&content));
-            // Save file
-            let output_path = Path::new("benches/output/test.bin");
-            std::fs::write(output_path, content).unwrap();
+            compress_fasta(black_box(&content));
         })
     });
-    c.bench_function("Large FASTA file", |b| {
+    c.bench_function("Compress Large FASTA file", |b| {
         b.iter(|| {
             let input_path = Path::new("benches/input/large.fasta");
             let content = black_box(std::fs::read_to_string(input_path).unwrap());
-            let content = compress_fasta(black_box(&content));
-            // Save file
-            let output_path = Path::new("benches/output/large.bin");
-            std::fs::write(output_path, content).unwrap();
+            compress_fasta(black_box(&content));
         })
     });
 }
